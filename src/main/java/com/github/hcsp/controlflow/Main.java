40315db0f8e6eb37742840ed69e33f2204074108
Leo Liu <leo.liu@domain.com.au>
2022-07-27 08:04:09 +1000

    feat/answer

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,8 +12,9 @@ public class Main {
     public static boolean isLeapYear(int year) {
         if (year % 100 == 0) {
             return year % 400 == 0;
-        } else
+        } else {
             return year % 4 == 0;
+        }
     }
 
     public static void main(String[] args) {
